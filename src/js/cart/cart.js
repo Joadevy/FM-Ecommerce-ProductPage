d59edc51a227ccb5a45d44134cb820c2c11fbf48
Joaquin Arlettaz <viiernes13@gmail.com>
2022-04-23 20:11:36 -0300

    Working with the cart display render

--- src/js/cart/cart.js
+++ src/js/cart/cart.js
@@ -5,11 +5,12 @@ import data from './products.json' assert {type: 'json'};
 const addCartBtn = document.getElementById('addToCart');
 const cartBtn = document.getElementById('cart-btn');
 const amountItems = document.getElementById('amount-items');
+const header = document.querySelector('.main');
 
 /* --------------- Event listeners --------------- */
 
 addCartBtn.addEventListener('click',()=>{addToCart(parseInt(amountItems.textContent),0)})
-cartBtn.addEventListener('click',()=>{console.log('CARRITO PADRINO')})
+cartBtn.addEventListener('click',()=>renderCart())
 
 /* --------------- Global variables --------------- */
 let cart = [];
@@ -28,51 +29,65 @@ function addToCart(amount,id){
             const item = data.find((product)=>product.id === id);
                 cart.push({
                     ...item,
-                    "numberOfUnits":1
+                    "numberOfUnits":amount
                 });
         }
         renderAmountCartItems();
-        updateCart();
     }
 }
 
 
-function updateCart(){
-
-}
-
-
 function renderAmountCartItems(){
-    console.log(cartBtn.parentElement)
-    // If not exists the container, then creates it
+    // If not exists the bubble container, then creates it
     if(cartBtn.parentElement.lastElementChild.classList !== 'user-menu__cart-bubble'){
         const bubble = document.createElement('div');
         bubble.classList.add('user-menu__cart-bubble');
         cartBtn.parentElement.appendChild(bubble);
     }
     let amountOfCartUnits = 0;
+    // Acumulating all of the units of the cart products.
     cart.forEach(product => {
-        amountOfCartUnits+=product.numberOfUnits // Acumulating all of the units of the cart products.
+        amountOfCartUnits+=product.numberOfUnits
     })
     console.log(amountOfCartUnits);
-    cartBtn.parentElement.lastElementChild.textContent = amountOfCartUnits // 
+    // Rendering the cart amount of units into the bubble.
+    cartBtn.parentElement.lastElementChild.textContent = amountOfCartUnits;
+    console.log(cart)
 }
 
-
-
-const cleanCart = () => {
+const cleanCart = (id) => {
+    // Removing the product for the array, it removes all the units of this type of.
+    cart.splice(id,1);
 }
 
 const renderCart = () => {
     let cart = createCart();
+    header.appendChild(cart);
 }
 
-const createCart = () => {
+function createCart(){
+    // Creating elements
     let cart = document.createElement('div');
-    let closebtn = document.createElement('button');
+    let header = document.createElement('div');
+    let title = document.createElement('p');
+    let closebtn = document.createElement('img');
     let product = document.createElement('div');
+    let imgproduct = document.createElement('img');
     let checkoutbtn = document.createElement('button');
-    return cart.append(closebtn,product,checkoutbtn);
+
+    // Adding the information
+    title.textContent = 'Cart';
+    closebtn.src = 'src/assets/icon/icon-close.svg';
+    imgproduct.src = "src/assets/images/image-product-0-thumbnail.jpg";
+
+    // Adding classes
+    cart.classList.add('cart-active');
+    cart.appendChild(imgproduct);
+
+    // Appending to their containers
+    header.append(title,closebtn);
+
+    return cart;
 }
 
 
